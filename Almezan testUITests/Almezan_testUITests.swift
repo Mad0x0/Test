b94a6890c506359e792fcbfda6f5c72c0f7a1f46
sh <sh@shs-mac-pro.local>
2017-04-26 22:55:23 +0200

    Initial Commit

---
+++ Almezan testUITests/Almezan_testUITests.swift
@@ -0,0 +1,36 @@
+//
+//  Almezan_testUITests.swift
+//  Almezan testUITests
+//
+//  Created by sh on 4/26/17.
+//  Copyright © 2017 sh. All rights reserved.
+//
+
+import XCTest
+
+class Almezan_testUITests: XCTestCase {
+        
+    override func setUp() {
+        super.setUp()
+        
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+        XCUIApplication().launch()
+
+        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // Use recording to get started writing UI tests.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+}
